fba6cfe7e78a13a834a09f84a9fc06183946a43f
michelfaiad <michelfaiad@gmail.com>
2021-05-12 09:50:58 -0300

    initial prototype

---
+++ Assets/Scripts/SprayController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SprayController : MonoBehaviour
+{
+
+    [Header("Object References")]
+    [SerializeField] ParticleSystem leftSpray;
+    [SerializeField] ParticleSystem rightSpray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            leftSpray.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            rightSpray.Play();
+        }
+    }
+}
